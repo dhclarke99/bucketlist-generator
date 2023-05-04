b75db0ef01f3e42221a44cb0df176cc26dac81d3
dhclarke99 <dhclarke99@gmail.com>
2023-05-03 20:47:43 -0400

    updated homeroutes and handlebars to display bucketlist data correctly

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,21 +8,16 @@ router.get('/', async (req, res) => {
       include: [{model:BucketListItem}],
     });
 
-  //   req.session.save(() => {
-  //     req.session.user_id = userData.id;
-  //     req.session.logged_in = true;
-
-      
-  //   });
-  
-    
     // Pass serialized data and session flag into template
     const listData = bucketListData.map((list) => list.get({ plain: true }));
+  
+
+    
     res.render('homepage', { 
-      listData, 
-    //   logged_in: req.session.logged_in 
+      listData
+    
     });
-    // res.status(200).json(bucketListData);
+   
   } catch (err) {
     res.status(400).json(err);
   }
@@ -58,25 +53,7 @@ router.get('/profile',  withAuth, async (req, res) => {
   }
 });
 
-// Use withAuth middleware to prevent access to route
-// router.get('/profile', withAuth, async (req, res) => {
-//   try {
-//     // Find the logged in user based on the session ID
-//     const userData = await User.findByPk(req.session.user_id, {
-//       attributes: { exclude: ['password'] },
-//       include: [{ model: Project }],
-//     });
 
-//     const user = userData.get({ plain: true });
-
-//     res.render('profile', {
-//       ...user,
-//       logged_in: true
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
--- public/js/homepage.js
+++ public/js/homepage.js
@@ -2,20 +2,15 @@
 
 const apiCall = async () => {
     console.log("click")
-    fetch('/', {
+    const response = await fetch('https://api.api-ninjas.com/v1/bucketlist', {
         method: 'GET',
-        url: 'https://api.api-ninjas.com/v1/bucketlist',
+        
         headers: { 'X-Api-Key': 'g4eBJJ1vStCoSNn4Hn+3Qg==dciOZWtnf9XNVu4g'},
         contentType: 'application/json',
-        success: function(result) {
-            console.log(result);
-        },
-        
-        error: function ajaxError(jqXHR) {
-            console.error('Error: ', jqXHR.responseText);
-        }
+       
     });
-    console.log(result)
+    const data = await response.json()
+    console.log(data)
 };
 
 
