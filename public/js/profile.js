007b6253b73687bb221660b03036a79a272af4f3
dhclarke99 <dhclarke99@gmail.com>
2023-05-03 23:36:42 -0400

    added generate button and fucntionality to profile page

---
+++ public/js/profile.js
@@ -0,0 +1,30 @@
+const apiCall = async () => {
+    console.log("click");
+  
+    const response = await fetch('https://api.api-ninjas.com/v1/bucketlist', {
+      method: 'GET',
+  
+      headers: { 'X-Api-Key': 'g4eBJJ1vStCoSNn4Hn+3Qg==dciOZWtnf9XNVu4g' },
+      contentType: 'application/json',
+  
+    });
+  
+    const data = await response.json();
+    console.log(data.item);
+  
+    const listContainer = document.querySelector('#profile-container');
+  
+    // Check if a child element exists
+    if (listContainer.querySelector('h2')) {
+        listContainer.querySelector('h2').textContent = data.item;
+      } else {
+        const listElement = document.createElement('div');
+        listElement.innerHTML = `<h2>${data.item}</h2>`;
+        listContainer.appendChild(listElement);
+      }
+  
+  };
+  
+  document
+    .querySelector('#profile-generate')
+    .addEventListener('click', apiCall);
