92955c401d87c40e19f2fc917a630df91b4f57ff
dhclarke99 <dhclarke99@gmail.com>
2023-05-03 22:10:20 -0400

    added api call data to the homepage after clicking generate button

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,21 +1,30 @@
-
-
 const apiCall = async () => {
-    console.log("click")
+    console.log("click");
+  
     const response = await fetch('https://api.api-ninjas.com/v1/bucketlist', {
-        method: 'GET',
-        
-        headers: { 'X-Api-Key': 'g4eBJJ1vStCoSNn4Hn+3Qg==dciOZWtnf9XNVu4g'},
-        contentType: 'application/json',
-       
+      method: 'GET',
+  
+      headers: { 'X-Api-Key': 'g4eBJJ1vStCoSNn4Hn+3Qg==dciOZWtnf9XNVu4g' },
+      contentType: 'application/json',
+  
     });
-    const data = await response.json()
-    console.log(data)
-};
-
-
-
-
-document
+  
+    const data = await response.json();
+    console.log(data.item);
+  
+    const listContainer = document.querySelector('#container');
+  
+    // Check if a child element exists
+    if (listContainer.querySelector('h2')) {
+        listContainer.querySelector('h2').textContent = data.item;
+      } else {
+        const listElement = document.createElement('div');
+        listElement.innerHTML = `<h2>${data.item}</h2>`;
+        listContainer.appendChild(listElement);
+      }
+  
+  };
+  
+  document
     .querySelector('#generate')
     .addEventListener('click', apiCall);
