71ea3a9cbc80890023360431420e6eae8fb8fb14
dhclarke99 <dhclarke99@gmail.com>
2023-07-16 02:53:30 -0400

    added functionality to delete entire bucketlist and connected to backend

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,5 +1,40 @@
 const listGenerator = document.querySelector('#list-generator');
   const landingPage = document.querySelector('#landing-page');
+  const deleteBtn = document.querySelectorAll(".delete-btn")
+
+
+
+  const deleteItem = async (event) => {
+    event.preventDefault();
+    const bucket_list_id = event.target.id
+    console.log(bucket_list_id)
+  
+    if (bucket_list_id) {
+      // Send a POST request to the API endpoint
+      const response = await fetch(`/api/bucketlists/${bucket_list_id}`, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+    
+      console.log(response)
+  
+      if (response.ok) {
+        
+        
+        // If successful, redirect the browser to the bucketlist page
+        document.location.replace(`/profile`);
+        console.log(id)
+      } else {
+        console.log(err)
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  deleteBtn.forEach(btn => {
+    btn.addEventListener("click", deleteItem)
+  })
 
 const createList = async (event) => {
 const title = document.querySelector('#title-create').value;
@@ -11,7 +46,7 @@ console.log(title)
   });
   console.log(JSON.stringify({title}))
   if (response.ok) {
-    listGenerator.setAttribute("style", "visibility:visible");
+  
     console.log("response worked")
      document.location.replace('/profile');
     // If successful, redirect the browser to the profile page
